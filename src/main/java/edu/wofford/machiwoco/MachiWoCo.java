5b9991fdbf187c66a57a400956a5f0e8f7a9ed69
MasterNobikan <vannoyjt@email.wofford.edu>
2020-10-15 19:51:35 -0400

    Added looping functionality to MachiWoco

--- src/main/java/edu/wofford/machiwoco/MachiWoCo.java
+++ src/main/java/edu/wofford/machiwoco/MachiWoCo.java
@@ -33,83 +33,84 @@ public class MachiWoCo {
         System.out.println("The game has started. Player 1 will go first.");
         
         //Loop begins
-        //Beginning of turn
-        if (p1.getTurn()) {
-            System.out.println("Turn started for Player 1.");
-        } else if (p2.getTurn()) {
-            System.out.println("Turn started for Player 2.");
-        }
-        
-        //Current Game State
-            // 1. Get Market state from Market Class
-            // 2. Get Players' state from Players Class
-        game.printMarketState();
-        game.printPlayerOneState();
-        game.printPlayerTwoState();
+            while (!p1.hasCityHall() || !p2.hasCityHall()) {
+            //Beginning of turn
+                if (p1.getTurn()) {
+                    System.out.println("Turn started for Player 1.");
+                } else if (p2.getTurn()) {
+                    System.out.println("Turn started for Player 2.");
+                }
+                
+                //Current Game State
+                    // 1. Get Market state from Market Class
+                    // 2. Get Players' state from Players Class
+                game.printMarketState();
+                game.printPlayerOneState();
+                game.printPlayerTwoState();
 
-        //Roll 
-            // 1. Player N rolls the dice and the result is displayed
-            // 2. Check to see if any cards are activated (pass dice to isActivated() method in GameState class)
-        int low = 1;
-        int high = 7;
-        int dice = random.nextInt(high - low) + low;
-        if (p1.getTurn()) {
-            System.out.println("Player 1 rolled [" + dice + "] = " + dice);
-        } else if (p2.getTurn()) {
-            System.out.println("Player 2 rolled [" + dice + "] = " + dice);
-        }
-        if (game.isActivated(dice) == true) {
-            if (dice == 1) {
-                System.out.println("Wheat Field activated for Player 1.");
-                System.out.println("Wheat Field activated for Player 2.");
-                p1.addCoins(1 * p1.getNumWheat()); //Times the number of Wheat Field cards that player has
-                p2.addCoins(1 * p2.getNumWheat()); // -----------------''--------------------
-            } else if (dice == 2) {  //And if one or both players own that card
-                if (p1.getNumRanch() > 0) {
-                    System.out.println("Ranch activated for Player 1.");
-                    p1.addCoins(1 * p1.getNumRanch());
+                //Roll 
+                    // 1. Player N rolls the dice and the result is displayed
+                    // 2. Check to see if any cards are activated (pass dice to isActivated() method in GameState class)
+                int low = 1;
+                int high = 7;
+                int dice = random.nextInt(high - low) + low;
+                if (p1.getTurn()) {
+                    System.out.println("Player 1 rolled [" + dice + "] = " + dice);
+                } else if (p2.getTurn()) {
+                    System.out.println("Player 2 rolled [" + dice + "] = " + dice);
                 }
-                if (p2.getNumRanch() > 0) {
-                    System.out.println("Ranch activated for Player 2.");
-                    p2.addCoins(1 * p2.getNumRanch());
+                if (game.isActivated(dice) == true) {
+                    if (dice == 1) {
+                        System.out.println("Wheat Field activated for Player 1.");
+                        System.out.println("Wheat Field activated for Player 2.");
+                        p1.addCoins(1 * p1.getNumWheat()); //Times the number of Wheat Field cards that player has
+                        p2.addCoins(1 * p2.getNumWheat()); // -----------------''--------------------
+                    } else if (dice == 2) {  //And if one or both players own that card
+                        if (p1.getNumRanch() > 0) {
+                            System.out.println("Ranch activated for Player 1.");
+                            p1.addCoins(1 * p1.getNumRanch());
+                        }
+                        if (p2.getNumRanch() > 0) {
+                            System.out.println("Ranch activated for Player 2.");
+                            p2.addCoins(1 * p2.getNumRanch());
+                        }
+                    } else if (dice == 5) {  //And if one ore both players own that card
+                        if (p1.getNumForest() > 0) {
+                            System.out.println("Forest activated for Player 1.");
+                            p1.addCoins(1 * p1.getNumForest());
+                        }
+                        if (p2.getNumForest() > 0) {
+                            System.out.println("Forest activated for Player 2.");
+                            p2.addCoins(1 * p2.getNumForest());
+                        }
+                        //p1.addCoins(1); if he has it and times how many
+                        //p2.addCoins(1); if he has it and times how many
+                    } 
                 }
-            } else if (dice == 5) {  //And if one ore both players own that card
-                if (p1.getNumForest() > 0) {
-                    System.out.println("Forest activated for Player 1.");
-                    p1.addCoins(1 * p1.getNumForest());
-                }
-                if (p2.getNumForest() > 0) {
-                    System.out.println("Forest activated for Player 2.");
-                    p2.addCoins(1 * p2.getNumForest());
-                }
-                //p1.addCoins(1); if he has it and times how many
-                //p2.addCoins(1); if he has it and times how many
-            } 
-        }
 
-        game.printMarketMenu();
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Choose a number to purchase or construct: ");
-        int choice = scan.nextInt();
-        scan.close();
-        // if else statements that checks what number the player entered...
-        // and then do add coins/remove card etc...
-        if (p1.getTurn()) {
-            System.out.println("Player 1 chose to..." + choice);
-        } else if (p2.getTurn()) {
-            System.out.println("Player 2 chose to..." + choice);
-        }
+                game.printMarketMenu();
+                Scanner scan = new Scanner(System.in);
+                System.out.println("Choose a number to purchase or construct: ");
+                int choice = scan.nextInt();
+                // if else statements that checks what number the player entered...
+                // and then do add coins/remove card etc...
+                if (p1.getTurn()) {
+                    System.out.println("Player 1 chose to..." + choice);
+                } else if (p2.getTurn()) {
+                    System.out.println("Player 2 chose to..." + choice);
+                }
 
-        //End turn for this player and then do this all again until the game ends...
-        if (p1.getTurn()) {
-            System.out.println("Turn ended for Player 1.");
-            p1.setTurn(false);
-            p2.setTurn(true);
-        } else if (p2.getTurn()) {
-            System.out.println("Turn ended for Player 2.");
-            p2.setTurn(false);
-            p1.setTurn(true);
-        }
+                //End turn for this player and then do this all again until the game ends...
+                if (p1.getTurn()) {
+                    System.out.println("Turn ended for Player 1.");
+                    p1.setTurn(false);
+                    p2.setTurn(true);
+                } else if (p2.getTurn()) {
+                    System.out.println("Turn ended for Player 2.");
+                    p2.setTurn(false);
+                    p1.setTurn(true);
+                }
+            }
         }
     }
 }
