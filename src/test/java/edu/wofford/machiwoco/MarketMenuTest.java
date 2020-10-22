cfa65ac0ace7fd5a5ebf1e0e7e3eb459a884fb63
MasterNobikan <vannoyjt@email.wofford.edu>
2020-10-22 12:08:19 -0400

    Added a test file for the MarketMenu class

---
+++ src/test/java/edu/wofford/machiwoco/MarketMenuTest.java
@@ -0,0 +1,13 @@
+package edu.wofford.machiwoco;
+
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.Matchers.is;
+
+
+public class MarketMenuTest {
+    @Test
+    public void testSomething() {
+        assertThat(1, is(1));
+    }
+}
