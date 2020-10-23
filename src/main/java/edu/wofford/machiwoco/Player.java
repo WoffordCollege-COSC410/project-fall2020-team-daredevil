e819a0744f7a4cc8235ea081567194e9bce9ab71
Evan Suggs <suggsec@gmail.com>
2020-10-22 22:15:26 -0400

    new terminal

--- src/main/java/edu/wofford/machiwoco/Player.java
+++ src/main/java/edu/wofford/machiwoco/Player.java
@@ -80,118 +80,115 @@ public class Player {
 	}
 		
 
-	/*
 
-	-------------------------------- OLD CODE ------------------------------------
 
-	int[] ecards;
-	private boolean cityHall;
-	
-	/**
-	 * Creates new instance of Player
-	 */
-	public Player() {
-		//1 starting card of Wheat
-		ecards = new int[3];
-		//array only stores num of cards for each type
-		ecards[0] = 1;
-		cityHall = false;
-		coins = 3;
-		myTurn = false;
-	}
+	// -------------------------------- OLD CODE ------------------------------------
 
-	/**
-	 * This method checks if Player N has constructed the City Hall.
-	 * @return cityHall 
-	 */
-	public boolean hasCityHall() {
-		return cityHall;
-	}
+	// int[] ecards;
+	// private boolean cityHall;
 	
-	/**
-	 * This method adds a card to Player N's inventory. 
-	 * @param s is the string "w", "r", "f", or "city" representing the 
-	 * card that will be given to player N.
-	 */
-	public void addCard(String s) {
-		if (s.equals("w") || s.equals("W")) { //Adding extra coins here?
-			ecards[0] = ecards[0] + 1;
-		} else if (s.equals("r") || s.equals("R")) {
-			ecards[1] = ecards[1] + 1;
-		} else if (s.equals("f") || s.equals("F")) {
-			ecards[2] = ecards[2] + 1;
-		} else if (s.equals("city")) {
-			cityHall = true;
-		}
-	}
+	// /**
+	//  * Creates new instance of Player
+	//  */
+	// public Player() {
+	// 	//1 starting card of Wheat
+	// 	ecards = new int[3];
+	// 	//array only stores num of cards for each type
+	// 	ecards[0] = 1;
+	// 	cityHall = false;
+	// 	coins = 3;
+	// 	myTurn = false;
+	// }
+
+	// /**
+	//  * This method checks if Player N has constructed the City Hall.
+	//  * @return cityHall 
+	//  */
+	// public boolean hasCityHall() {
+	// 	return cityHall;
+	// }
 	
-	/**
-	 * This method gets the amount of Wheat Field cards Player N has.
-	 * @return ecards[0]
-	 */
-	public int getNumWheat(){
-		return ecards[0];
-	}
+	// /**
+	//  * This method adds a card to Player N's inventory. 
+	//  * @param s is the string "w", "r", "f", or "city" representing the 
+	//  * card that will be given to player N.
+	//  */
+	// public void addCard(String s) {
+	// 	if (s.equals("w") || s.equals("W")) { //Adding extra coins here?
+	// 		ecards[0] = ecards[0] + 1;
+	// 	} else if (s.equals("r") || s.equals("R")) {
+	// 		ecards[1] = ecards[1] + 1;
+	// 	} else if (s.equals("f") || s.equals("F")) {
+	// 		ecards[2] = ecards[2] + 1;
+	// 	} else if (s.equals("city")) {
+	// 		cityHall = true;
+	// 	}
+	// }
 	
-	/**
-	 * This method gets the amount of Ranch cards Player N has.
-	 * @return ecards[1]
-	 */
-	public int getNumRanch(){
-		return ecards[1];
-	}
+	// /**
+	//  * This method gets the amount of Wheat Field cards Player N has.
+	//  * @return ecards[0]
+	//  */
+	// public int getNumWheat(){
+	// 	return ecards[0];
+	// }
 	
-	/**
-	 * This method gets the amount of Forest cards Player N has.
-	 * @return ecard[2]
-	 */
-	public int getNumForest(){
-		return ecards[2];
-	}
+	// /**
+	//  * This method gets the amount of Ranch cards Player N has.
+	//  * @return ecards[1]
+	//  */
+	// public int getNumRanch(){
+	// 	return ecards[1];
+	// }
 	
-	/**
-	 * This method gets the turn of Player N.
-	 * @return myTurn
-	 */
-	public boolean getTurn() {
-		return myTurn;
-	}
+	// /**
+	//  * This method gets the amount of Forest cards Player N has.
+	//  * @return ecard[2]
+	//  */
+	// public int getNumForest(){
+	// 	return ecards[2];
+	// }
 	
-	/**
-	 * @param b is true or false to set
-	 * This method sets the turn for Player N.
-	 * @param b is a boolean value that sets player N's turn.
-	 */
-	public void setTurn(boolean b) {
-		myTurn = b;
-	}
+	// /**
+	//  * This method gets the turn of Player N.
+	//  * @return myTurn
+	//  */
+	// public boolean getTurn() {
+	// 	return myTurn;
+	// }
 	
-	/**
-	 * This method gets the amount of coins Player N has.
-	 * @return coins
-	 */
-	public int getCoins() {
-		return coins;
-	}
+	// /**
+	//  * @param b is true or false to set
+	//  * This method sets the turn for Player N.
+	//  * @param b is a boolean value that sets player N's turn.
+	//  */
+	// public void setTurn(boolean b) {
+	// 	myTurn = b;
+	// }
 	
-	/**
-	 * This method adds coins to Player N's inventory.
-	 * @param c is the int value that Player N's coins should increase.
-	 */
-	public void addCoins(int c) {
-		coins = c + coins;
-	}
+	// /**
+	//  * This method gets the amount of coins Player N has.
+	//  * @return coins
+	//  */
+	// public int getCoins() {
+	// 	return coins;
+	// }
 	
-	/**
-	 * This method removes coins from Player N's inventory.
-	 * @param c is the int value that Player N's coins should decrease.
-	 */
-	public void removeCoins(int c) {
-		coins = coins - c;
-	}
+	// /**
+	//  * This method adds coins to Player N's inventory.
+	//  * @param c is the int value that Player N's coins should increase.
+	//  */
+	// public void addCoins(int c) {
+	// 	coins = c + coins;
+	// }
 	
-//	public static void main(String[] args) {
-//		Player p1 = new Player();
-//		System.out.println(p1.getLastCard());
-//	}
+	// /**
+	//  * This method removes coins from Player N's inventory.
+	//  * @param c is the int value that Player N's coins should decrease.
+	//  */
+	// public void removeCoins(int c) {
+	// 	coins = coins - c;
+	// }
+
+
 }
